b4586836ec2e00a0bc41e5eda2ebba4d8557486e
andy <bunce.andy@gmail.com>
2014-04-10 11:46:19 +0100

    tidy

--- examples/StreamAddExample.js
+++ examples/StreamAddExample.js
@@ -7,13 +7,14 @@ var log = require("../debug");
 var fs = require("fs");
 
 // create session
-var client = new basex.Session("localhost", 1984, "admin", "admin");
+var client = new basex.Session();
 basex.debug_mode = false;
+
 // create new database
  client.execute("create db test_db", log.print);
- var s=fs.createReadStream(__dirname+ "\\books.xml");
-
-// add document
+ 
+// add document from stream
+ var s=fs.createReadStream(__dirname+ "\\books.xml"); 
 client.add("/world/World.xml", s, log.print);
 
 // drop database
