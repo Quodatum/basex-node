a351231ff161f2ea188e119e9588f00445247cdf
salim-dev <elmoussaoui.salim.dev@gmail.com>
2021-09-29 12:51:15 +0200

    clearTimeout

--- examples/issue22.js
+++ examples/issue22.js
@@ -5,25 +5,37 @@ const basex = require("../index");
 
 basex.debug_mode = false;
 var session;
-
+var timerknockSocketError = false;
+var timerknockWork = false;
 
 function  socketError(e){
+if (timerknockWork) {   
+    console.log("================ clearTimeout timerknockWork ======================");
+    clearTimeout(timerknockWork);
+  }
+  
   console.log("socket trouble detected..",e.code);
   if (e.code == 'ECONNREFUSED') {
-    console.error('connection refused. Check BaseX server is running.');
+    console.error('connection refused. Check BaseX server is running. Restarting client in 15 seconds');
+    timerknockSocketError = setTimeout(work, 15000); // 15 seconds pass..
   } else if(e.code == 'ECONNRESET' || e.code == 'EPIPE'){
     console.log("Restarting client in 10 seconds");
-    setTimeout(work, 10000); // 10 seconds pass..
+    timerknockSocketError = setTimeout(work, 10000); // 10 seconds pass..
   } else {
     console.error("SOCKET ERROR: ", e.code,":", e);
   }
 };
 function work(){
-  console.log("New session");
+console.log("New session");
+  if (timerknockSocketError) {
+      // some statement
+      console.log("================ clearTimeout timerknock SocketError ======================");
+      clearTimeout(timerknockSocketError);
+    }
   session = new basex.Session();
   session.on("socketError",socketError);
   // poll server time every second
-  setInterval(serverdateTime,1000);
+  timerknockWork = setInterval(serverdateTime,1000);
 };
 
 
